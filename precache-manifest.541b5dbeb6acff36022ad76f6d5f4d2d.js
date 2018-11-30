e70f14113418e5e499937c2704b9da1f0f519ae2
Walker Twyman <walk12288@gmail.com>
2018-11-30 13:57:25 -0600

    Updates

---
+++ precache-manifest.541b5dbeb6acff36022ad76f6d5f4d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa61da13b5cf674bd140",
+    "url": "/react-hot-cold-game/static/js/runtime~main.fa61da13.js"
+  },
+  {
+    "revision": "440de0c6ed0bd3c52721",
+    "url": "/react-hot-cold-game/static/js/main.440de0c6.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/react-hot-cold-game/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "440de0c6ed0bd3c52721",
+    "url": "/react-hot-cold-game/static/css/main.6383ff2f.chunk.css"
+  },
+  {
+    "revision": "fe7da3de4a14ade0429f8e069341826e",
+    "url": "/react-hot-cold-game/index.html"
+  }
+];
